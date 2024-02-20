a7188969f0d4633b50175780b439b5c2177077d3
MoldyLunchBox <duu116@gmail.com>
2024-02-20 17:25:14 +0100

    [+] search for and fetch users

---
+++ swifty-companion/utils/searchUsers.js
@@ -0,0 +1,25 @@
+import { handleTokenExpiration } from "./tokenFunctions";
+import axios from "axios";
+import { FETCH_USER_URL} from "@env";
+
+export const fetchUser = async (user, dispatch) => {
+    const { tokenInfo } = await handleTokenExpiration(dispatch);
+    const { access_token } = tokenInfo;
+    return new Promise(async (resolve, reject) => {
+      return await axios
+        .get(FETCH_USER_URL + user, {
+          headers: { Authorization: `Bearer ${access_token}` },
+        })
+        .then((value) => {
+          const data = {}
+          const strings =  ['login', 'displayname', 'kind', 'location']
+          console.log('=========')
+          strings.map(key=>  data[key] = value.data[key] )
+          resolve(data);
+        })
+        .catch((error) => {
+          console.log("error:error:", error);
+          reject("User not found!:", error);
+        });
+    });
+  };
