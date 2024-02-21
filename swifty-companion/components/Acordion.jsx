5e3622cdd67eaddc9bd8a01696cb79067c5c7a45
MoldyLunchBox <duu116@gmail.com>
2024-02-22 00:42:04 +0100

    [+] created accordions for projects and skills .

---
+++ swifty-companion/components/Acordion.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import ProjectsAcordionContents from './ProjectsAcordionContents';
+
+const Acordion = ({ panel, togglePanel }) => {
+    return (
+        <View key={panel.id} style={panelsStyle.panel}>
+            {/* Header */}
+            <TouchableOpacity onPress={() => togglePanel(panel.id)} style={panelsStyle.header}>
+                <Text style={panelsStyle.headerText}>{panel.title}</Text>
+                <Text>{panel.isExpanded ? '▼' : '▶'}</Text>
+            </TouchableOpacity>
+
+            {/* Content */}
+            {panel.isExpanded && (
+                <ScrollView style={panelsStyle.content}>
+                    <Text  style={{width:'100%', backgroundColor:'#ff0000'}}>
+                        {
+                            panel.title == 'Projects'
+                                ?
+                                <ProjectsAcordionContents projects={panel.content}/>
+                                :
+                                'no'
+                        }
+                    </Text>
+
+                </ScrollView>
+            )}
+        </View>
+    )
+}
+
+const panelsStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: 'lightblue',
+
+    },
+    panel: {
+        backgroundColor: '#ffffff',
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        borderRadius: 5,
+        overflow: 'hidden',
+        marginBottom: 10,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        backgroundColor: '#f0f0f0',
+    },
+    headerText: {
+        fontWeight: 'bold',
+    },
+    content: {
+        padding: 10,
+        maxHeight: 200, // Adjust as needed
+    },
+});
+
+export default Acordion
